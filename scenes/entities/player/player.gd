be07df1222862e8f939942a2e032adee1116c5f4
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-04-09 19:59:33 -0400

    addd camera rotation and relative movement to the palyer

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -2,10 +2,12 @@ extends CharacterBody3D
 
 @export var base_speed : float = 4.0
 
+@onready var camera = $CameraController/Camera3D
+
 var movement_input : Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	movement_input = Input.get_vector("left", "right", "forward", "backward")
+	movement_input = Input.get_vector("left", "right", "forward", "backward").rotated(-camera.global_rotation.y)
 	velocity = Vector3(movement_input.x, 0, movement_input.y) * base_speed
 	
 	move_and_slide()
