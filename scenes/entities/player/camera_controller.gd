0e0288a936b64be938c81300be7fa677c05bcfb3
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-04-09 11:30:37 -0400

    added comments to the camera controller

--- scenes/entities/player/camera_controller.gd
+++ scenes/entities/player/camera_controller.gd
@@ -4,7 +4,11 @@ extends Node3D
 @export var vertical_acceleration : float = 1.0
 
 func _process(delta: float) -> void:
+	
+	# input vector for joystick 
 	var joy_dir = Input.get_vector("pan_left", "pan_right", "pan_up", "pan_down")
+	
+	# rotate the joystick vector
 	rotate_from_vector(joy_dir * delta * Vector2(horizontal_acceleration, vertical_acceleration))
 
 func _input(event: InputEvent) -> void:
@@ -14,6 +18,10 @@ func _input(event: InputEvent) -> void:
 
 func rotate_from_vector(v: Vector2) -> void:
 	if v.length() == 0 : return
+	
+	# rotate horizontally along the y axis
 	rotation.y += v.x
+	
+	# rotate vertically along the x axis and clamp the rotation
 	rotation.x += v.y
 	rotation.x = clampf(rotation.x, -0.8, 0.1)
