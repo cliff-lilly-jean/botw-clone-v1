0fda35c6d03b539b7eb35f0112ee740bd8053502
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-04-26 18:11:51 -0400

    got the chop animation to only work afte the animation is done playing and not continuosly when pressing the button

--- scenes/entities/player/peasant_skin.gd
+++ scenes/entities/player/peasant_skin.gd
@@ -5,9 +5,16 @@ extends Node3D
 
 @onready var movement_state_machine = animation_tree.get("parameters/MoveStateMachine/playback")
 
+var attacking: bool = false
 
 func set_movement_state(state : String) -> void:
 	movement_state_machine.travel(state)
 
 func attack() -> void:
-	animation_tree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	if not attacking:
+		animation_tree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+
+func attack_toggle(value: bool) -> void:
+	attacking = value
+	print(attacking)
