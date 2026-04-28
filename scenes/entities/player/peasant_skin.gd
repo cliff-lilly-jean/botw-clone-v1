e63b62f7077f869051d407c2f76aaa62747306ae
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-04-27 22:48:36 -0400

    updated animations

--- scenes/entities/player/peasant_skin.gd
+++ scenes/entities/player/peasant_skin.gd
@@ -2,8 +2,10 @@ extends Node3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_tree: AnimationTree = $AnimationTree
+@onready var second_attack_timer: Timer = $SecondAttackTimer
 
 @onready var movement_state_machine = animation_tree.get("parameters/MoveStateMachine/playback")
+@onready var attack_state_machine = animation_tree.get("parameters/AttackStateMachine/playback")
 
 var attacking: bool = false
 
@@ -12,9 +14,9 @@ func set_movement_state(state : String) -> void:
 
 func attack() -> void:
 	if not attacking:
+		attack_state_machine.travel('Slice' if second_attack_timer.time_left else 'Chop')
 		animation_tree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 
 
 func attack_toggle(value: bool) -> void:
 	attacking = value
-	print(attacking)
